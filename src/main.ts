048dee7703a5ca85896ff0cd6fc4d4193eedf9e9
Pranav Tartey <pranavtartey@Apex-Doom.local>
2026-02-23 20:15:54 +0530

    fix: added the 0.0.0.0 access port for deployment

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
   console.log(`App running at http://localhost:${process.env.PORT ?? 8080}`);
   console.log(`Swagger docs at http://localhost:${process.env.PORT ?? 8080}/api/docs`);
 }
